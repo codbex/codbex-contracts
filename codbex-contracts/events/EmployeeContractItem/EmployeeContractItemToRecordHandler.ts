e55ff7a4ad623a6a83583d5141f5e79a6740398c
NicoleNG18 <nicole.georgieva18@abv.bg>
2024-09-16 19:19:53 +0300

    add more documents to the contract

--- codbex-contracts/events/EmployeeContractItem/EmployeeContractItemToRecordHandler.ts
+++ codbex-contracts/events/EmployeeContractItem/EmployeeContractItemToRecordHandler.ts
@@ -9,6 +9,16 @@ export const trigger = (event) => {
 
     if (operation === "create") {
 
+        const laborCodeNotification = {
+            "Name": "Notification under Article 62, Paragraph 5 of the Labor Code",
+            "EmployeeContract": contractItem.Id
+        }
+
+        const jobDescription = {
+            "Name": "Job Description",
+            "EmployeeContract": contractItem.Id
+        }
+
         const medicalCertificate = {
             "Name": "Medical Certificate",
             "EmployeeContract": contractItem.Id
@@ -19,16 +29,41 @@ export const trigger = (event) => {
             "EmployeeContract": contractItem.Id
         }
 
+        const initialSafetyBriefing = {
+            "Name": "Initial Safety Briefing",
+            "EmployeeContract": contractItem.Id
+        }
+
+        const safetyBriefingFor2024 = {
+            "Name": "Safety Briefing for 2024",
+            "EmployeeContract": contractItem.Id
+        }
+
         const criminalRecordCerificate = {
             "Name": "Criminal Record Cerificate",
             "EmployeeContract": contractItem.Id
         }
 
-        console.log("here");
+        const safetyDeclaration = {
+            "Name": "Safety Declaration",
+            "EmployeeContract": contractItem.Id
+        }
 
+        const safetyQuestionnaire = {
+            "Name": "Safety Questionnaire",
+            "EmployeeContract": contractItem.Id
+        }
+
+        EmployeeContractItemDao.create(laborCodeNotification);
+        EmployeeContractItemDao.create(jobDescription);
         EmployeeContractItemDao.create(medicalCertificate);
         EmployeeContractItemDao.create(bankAccountDetails);
+        EmployeeContractItemDao.create(initialSafetyBriefing);
+        EmployeeContractItemDao.create(safetyBriefingFor2024);
         EmployeeContractItemDao.create(criminalRecordCerificate);
+        EmployeeContractItemDao.create(safetyQuestionnaire);
+        EmployeeContractItemDao.create(safetyDeclaration);
+
     }
 
 }
