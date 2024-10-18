4c82722fbc195980d10cd48a7fd73b877d715abf
TIVMOF <tomyivanov06@gmail.com>
2024-10-18 06:03:44 +0000

    feat: Added contracts navigation

---
+++ codbex-contracts/navigations/services/contracts.js
@@ -0,0 +1,19 @@
+const navigationData = {
+    id: 'contracts-navigation',
+    label: "Contracts",
+    view: "contracts assignments",
+    group: "configurations",
+    orderNumber: 1000,
+    lazyLoad: true,
+    link: "/services/web/codbex-contracts/gen/codbex-contracts/ui/Contract/index.html?embedded"
+};
+
+function getNavigation() {
+    return navigationData;
+}
+
+if (typeof exports !== 'undefined') {
+    exports.getNavigation = getNavigation;
+}
+
+export { getNavigation }
